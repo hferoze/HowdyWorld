c4b16dbaebff1d943060d6dc18be563ab55600f0
hferoze <hassan.feroze@gmail.com>
2015-06-21 17:25:31 -0700

    Master Commit

---
+++ app/src/main/java/com/funtuts/android/coreinit/howdyworld/MainActivity.java
@@ -0,0 +1,36 @@
+package com.funtuts.android.coreinit.howdyworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button searchBtn;
+    WebView searchPg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        searchPg = (WebView) findViewById(R.id.searchWV);
+        searchPg.getSettings().setJavaScriptEnabled(true);
+
+        searchBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                searchPg.loadUrl("https://www.google.com/?gws_rd=ssl");
+            }
+        }
+
+        );
+    }
+
+
+}
